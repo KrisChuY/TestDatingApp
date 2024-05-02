b153aaecdf7bb7cd05b13a0288feb91fef4bc759
KrisChuY <132450976+KrisChuY@users.noreply.github.com>
2024-05-02 02:44:39 +0200

    Add files via upload

---
+++ Dating app - Free UI Kit (Community) (Copy) (Copy)1/data/story.js
@@ -0,0 +1,317 @@
+var story = {
+ "docName": "Dating app - Free UI Kit (Community) (Copy) (Copy)",
+ "docPath": "P_P_P",
+ "docVersion": 100000001,
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "fileType": "png",
+ "disableInteractions": false,
+ "highlightHotspot": true,
+ "highlightAllHotspots": true,
+ "hideGallery": false,
+ "galleryPageColorsEnabled": true,
+ "galleryDecorNodesEnabled": true,
+ "zoomEnabled": true,
+ "cloud": false,
+ "title": "Dating app - Free UI Kit (Community) (Copy) (Copy)",
+ "layersExist": true,
+ "pages": [
+  {
+   "id": "309:5423",
+   "groupIndex": 0,
+   "title": "Main",
+   "image": "main.png",
+   "index": 0,
+   "width": 375,
+   "height": 812,
+   "x": 0,
+   "y": 2886,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "content",
+     "rect": {
+      "x": 40,
+      "y": 120,
+      "width": 295,
+      "height": 466
+     },
+     "index": 0,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "309:5996",
+   "groupIndex": 0,
+   "title": "Filters",
+   "image": "filters.png",
+   "index": 1,
+   "width": 375,
+   "height": 812,
+   "x": 0,
+   "y": 3848,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Filter popup",
+     "rect": {
+      "x": 0,
+      "y": 163,
+      "width": 375,
+      "height": 649
+     },
+     "index": 1,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "309:5768",
+   "groupIndex": 0,
+   "title": "Swipe right",
+   "image": "swipe-right.png",
+   "index": 2,
+   "width": 375,
+   "height": 812,
+   "x": 525,
+   "y": 2886,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "swipe right",
+     "rect": {
+      "x": 82,
+      "y": 100,
+      "width": 401.41766357421875,
+      "height": 511.0192565917969
+     },
+     "index": 2,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 4,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "309:5586",
+   "groupIndex": 0,
+   "title": "Matches",
+   "image": "matches.png",
+   "index": 3,
+   "width": 375,
+   "height": 812,
+   "x": 525,
+   "y": 3848,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "nav bar",
+     "rect": {
+      "x": 0,
+      "y": 730,
+      "width": 375,
+      "height": 48
+     },
+     "index": 3,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "360:5",
+   "groupIndex": 0,
+   "title": "Swipe left",
+   "image": "swipe-left.png",
+   "index": 4,
+   "width": 375,
+   "height": 812,
+   "x": 1050,
+   "y": 2886,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Group 26",
+     "rect": {
+      "x": -109.00006103515625,
+      "y": 100,
+      "width": 401.41766357421875,
+      "height": 511.0192565917969
+     },
+     "index": 4,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 6,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "309:5476",
+   "groupIndex": 0,
+   "title": "Messages",
+   "image": "messages.png",
+   "index": 5,
+   "width": 375,
+   "height": 812,
+   "x": 1050,
+   "y": 3848,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "2 Messages",
+     "rect": {
+      "x": 40,
+      "y": 312,
+      "width": 295,
+      "height": 433
+     },
+     "index": 5,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 7,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "309:5749",
+   "groupIndex": 0,
+   "title": "Match",
+   "image": "match.png",
+   "index": 6,
+   "width": 375,
+   "height": 812,
+   "x": 1575,
+   "y": 2886,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "309:5683",
+   "groupIndex": 0,
+   "title": "Chat",
+   "image": "chat.png",
+   "index": 7,
+   "width": 375,
+   "height": 812,
+   "x": 1575,
+   "y": 3848,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "309:5987",
+   "groupIndex": 0,
+   "title": "Photo fullsreen",
+   "image": "photo-fullsreen.png",
+   "index": 8,
+   "width": 375,
+   "height": 812,
+   "x": 1575,
+   "y": 4810,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [],
+   "layout": null,
+   "tmpSolitBackground": false
+  }
+ ],
+ "startPageIndex": 0,
+ "totalImages": 9,
+ "groups": [
+  {
+   "id": "1:2",
+   "index": 0,
+   "name": "UI-Kit",
+   "backColor": "#E5E5E5"
+  }
+ ]
+}
